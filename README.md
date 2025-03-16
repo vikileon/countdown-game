1a3d4a9d6e9cc03de5875fec2938da97372dc1ee
Daria <daria.tim1@yahoo.com>
2025-03-16 21:55:53 +0300

    first commit. Implemented design and core game logic

---
+++ README.md
@@ -0,0 +1,14 @@
+# The Almost Final Countdown
+
+![Overview](./src/assets/overview.png)
+
+## Description
+
+A simple reaction training game.
+
+## Rules
+
+You must stop the timer before it expires. The closer you are to the expiration, the better!  
+The challenge is that you can't see the timer, so it's all about your sense of time and reaction.
+
+Good luck!
---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,38 @@
+import { useRef, useState } from "react";
+
+export default function TimerChallenge({ title, targetTime }) {
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  const timer = useRef();
+
+  function handleStart() {
+    setTimerStarted(!timerStarted);
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+  }
+
+  function handleStop() {
+    setTimerStarted(!timerStarted);
+    clearTimeout(timer.current);
+  }
+
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
+      <p className="challenge-time">
+        {targetTime} second{targetTime > 1 ? "s" : ""}
+      </p>
+      <p>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? "Stop" : "Start"} Challenge
+        </button>
+      </p>
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? "Time is running..." : "Timer inactive"}
+      </p>
+    </section>
+  );
+}
